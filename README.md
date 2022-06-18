022175aa0d3d38c43733b108804ab1ec4761fe65
tblandin <tiarra.blandin@outlook.com>
2022-06-18 14:12:10 -0600

    Sat Homework

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # MakeChangeProject
 
 ##Description
+	Let the teller know exact change from product purchase.
 
 ##Technologies Used
+	scanner and if statements.
 
 ##What I Learned
-
+	To multiply by 100 to not get a super long decimal number. I also learned to 	use the fall through concept with numerous if statements instead of using 		else if statements.
--- src/com/skilldistillery/makechange/MakeChange.java
+++ src/com/skilldistillery/makechange/MakeChange.java
@@ -5,7 +5,79 @@ import java.util.Scanner;
 public class MakeChange {
 
 	public static void main(String[] args) {
-		Scanner
+		Scanner sc = new Scanner(System.in);
+		
+		double price = 0.00;
+		double cash = 0.00;
+		double dollars = 0.00;
+		double cents = 0.00;
+		
+		
+		System.out.println("Price: ");
+		price  = sc.nextDouble();
+		
+		System.out.println("Cash: ");
+		cash  = sc.nextDouble();
+		
+		if (cash < price) {
+			System.out.println("Not enough cash. Goodbye.");
+		}
+		
+		else if (cash > price) {
+			dollars = cash - price;
+//			cents = dollars % 2;
+			System.out.println("\nChange: \n");
+				if ((int)dollars / 20 > 0) {
+					System.out.println("20's: " + ((int)dollars / 20));
+					dollars %= 20; 
+				}
+				
+				if ((int)dollars / 10 > 0) {
+					System.out.println("10's: " + (int)dollars / 10);
+					dollars %= 10; 
+				}
+				
+				if ((int)dollars / 5 > 0) {
+					System.out.println("5's: " + (int)dollars / 5);
+					dollars %= 5; 
+				}
+				
+				if ((int)dollars / 1 > 0) {
+					System.out.println("1's: " + (int)dollars / 1);
+					dollars %= 1; 
+					dollars *= 100;
+				}
+				
+				if ((int)dollars / 25 > 0) {
+					System.out.println("Quarters: " + (int)dollars / 25);
+					dollars %= 25; 
+				}
+				
+				if ((int)dollars / 10 > 0) {
+					System.out.println("Dimes: " + (int)dollars / 10);
+					dollars %= 10; 
+				}
+				
+				if ((int)dollars / 5 > 0) {
+					System.out.println("Nickels: " + (int)dollars / 5);
+					dollars %= 5; 
+				}
+				
+				if ((int)dollars / 1 > 0) {
+					System.out.println("Pennies: " + (int)dollars / 1);
+					dollars %= 1; 
+				}
+				
+			System.out.println("\nThank you. Goodbye.");
+			
+		}
+		
+		else if (cash == price) {
+			System.out.println("Thank you. Goodbye.");
+		}
+		
+		sc.close();
+		
 	}
 
 }
